7c076c8d9089f529f3413646d834573e9b46ef6e
Allan Villanueva <84660320+ko-allan@users.noreply.github.com>
2022-08-22 12:12:34 -0700

    Update with comments

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,7 +2,11 @@ from pathlib import Path
 
 LOG_DIR = "/Users/adcv/airflow/logs"
 
-def analyze_file(file_list):
+def analyze_file(file_list: str):
+"""
+    The analyze_file method is the main method of this script. 
+    
+"""
     error_count = 0
     error_messages = []
 
@@ -10,19 +14,24 @@ def analyze_file(file_list):
         current_file = open(str(file), "r")
 
         for line in current_file.readlines():
-            contents = line.split(" ")
+            contents = line.split(" ") # delimiter in logs are spaces
             for word in contents:
                 if word in error:
                     error_messages.append(line)
                     error_count += 1
 
+    # as specified in requirements
     return error_count, error_messages
 
-
+# grabs every .log file in Path specified
 file_list = Path(LOG_DIR).rglob("*.log")
+# can include more errors in this variable
+# ex: ["ERROR","WARNING"]
 error = ["ERROR"]
 
+#as specified in requirements
 count, cur_list = analyze_file(file_list)
+
 print("Total number of errors: " + str(count))
 print("Here are all the errors:")
 for error in cur_list:
